5fe99e953af3d99448244205316625adfd54080b
Sergey Dmitrenko <dmmytrenko@gmail.com>
2022-05-27 15:44:55 +0300

    add entities

---
+++ Entities/Models/Blog.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Blog
+    {
+        [Column("BlogId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Title is a required field.")]
+        [MinLength(5, ErrorMessage = "Minimum length for title is 5 characters")]
+        [MaxLength(50, ErrorMessage = "Maximum length for title is 50 characters")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Text is a required field.")]
+        [MinLength(5, ErrorMessage = "Minimum length for title is 5 characters")]
+        public string? Text { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ Entities/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class User
+    {
+        [Column("UserId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "User name is a required field.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "User surname is a required field.")]
+        public string? Surname { get; set; }
+
+        [Required(ErrorMessage = "Nickname is a required field.")]
+        [MaxLength(15, ErrorMessage = "Maximum length for nickname is 15 characters.")]
+        public string? Nickname { get; set; }
+
+        public DateTime DateRegistration { get { return DateTime.Now; } }
+    }
+}
