41f324918aa037dd237082639da1aa66257ef5b7
Sergey Dmitrenko <dmmytrenko@gmail.com>
2022-05-27 17:10:04 +0300

    create repository project

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Blog>? Blogs { get; set; }
+    }
+}
