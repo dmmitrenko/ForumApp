7113086d08706061c05d01645ccac27f04ce053d
Sergey Dmitrenko <dmmytrenko@gmail.com>
2022-05-30 13:03:49 +0300

    Configure automapper

---
+++ Web/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DTO;
+
+namespace Web
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>().ForMember(c => c.DateRegistration,
+                opt => opt.MapFrom(x => x.DateRegistration.ToString()));
+        }
+    }
+}
