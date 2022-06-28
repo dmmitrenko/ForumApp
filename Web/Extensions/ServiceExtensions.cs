80416ee653d6af1bc4ed049609bd403cf0ab6cab
Sergey Dmitrenko <dmmytrenko@gmail.com>
2022-06-28 20:45:46 +0300

    authorization with swagger

--- Web/Extensions/ServiceExtensions.cs
+++ Web/Extensions/ServiceExtensions.cs
@@ -102,6 +102,32 @@ public static class ServiceExtensions
         services.AddSwaggerGen(s =>
         {
             s.SwaggerDoc("v1", new OpenApiInfo { Title = "Forum App API", Version = "v1" });
+
+            s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Place to add JWT with Bearer",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Name = "Bearer",
+                    },
+                    new List<string>()
+                }
+            });
         });
+
     }
 }
